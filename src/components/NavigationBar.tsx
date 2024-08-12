f49c454831ae29461e88697673fed0442cfb668d
koladeolukoya <koladeolukoya@gmail.com>
2024-08-12 18:36:39 +0100

    style: added discord icon and nav responsive

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -12,7 +12,7 @@ export const NavigationBar = () => {
     <nav className="border-gray-200 bg-white">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
-          href="https://flowbite.com/"
+          href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <Image
@@ -23,45 +23,66 @@ export const NavigationBar = () => {
             alt="Decatron 360"
           />
         </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex md:order-2 space-x-3 rtl:space-x-reverse">
           <button
             type="button"
             className="flex items-center text-white text-sm rounded-lg px-5 py-2 bg-primary hover:bg-primary-dark transition-colors"
           >
-            <span className="mr-2">Join Community</span>
+            <span className="hidden sm:inline-block mr-2">Join Community</span>
+            <svg 
+              className="w-5 h-5"
+              viewBox="0 0 192 192" 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none"><path stroke="#ffff" 
+              stroke-linecap="round" 
+              stroke-linejoin="round" 
+              stroke-width="12" 
+                d="m68 138-8 16c-10.19-4.246-20.742-8.492-31.96-15.8-3.912-2.549-6.284-6.88-6.378-11.548-.488-23.964 5.134-48.056 19.369-73.528 1.863-3.334 4.967-5.778 8.567-7.056C58.186 43.02 64.016 40.664 74 39l6 11s6-2 16-2 16 2 16 2l6-11c9.984 1.664 15.814 4.02 24.402 7.068 3.6 1.278 6.704 3.722 8.567 7.056 14.235 25.472 19.857 49.564 19.37 73.528-.095 4.668-2.467 8.999-6.379 11.548-11.218 7.308-21.769 11.554-31.96 15.8l-8-16m-68-8s20 10 40 10 40-10 40-10"/><ellipse cx="71" cy="101" fill="#ffff" rx="13" ry="15"
+              />
+              <ellipse 
+                cx="121" 
+                cy="101" 
+                fill="#ffff" 
+                rx="13" 
+                ry="15"
+              />
+            </svg>
+          </button>
+          <button
+            type="button"
+            onClick={toggleMenu}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
             <svg
               className="w-5 h-5"
-              viewBox="0 0 32 32"
-              fill="none"
-              stroke="currentColor"
+              aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
             >
               <path
-                d="M2 11.6C2 8.23969 2 6.55953 2.65396 5.27606C3.2292 4.14708 4.14708 3.2292 5.27606 2.65396C6.55953 2 8.23969 2 11.6 2H20.4C23.7603 2 25.4405 2 26.7239 2.65396C27.8529 3.2292 28.7708 4.14708 29.346 5.27606C30 6.55953 30 8.23969 30 11.6V20.4C30 23.7603 30 25.4405 29.346 26.7239C28.7708 27.8529 27.8529 28.7708 26.7239 29.346C25.4405 30 23.7603 30 20.4 30H11.6C8.23969 30 6.55953 30 5.27606 29.346C4.14708 28.7708 3.2292 27.8529 2.65396 26.7239C2 25.4405 2 23.7603 2 20.4V11.6Z"
-                strokeWidth="2"
-                strokeMiterlimit="10"
+                stroke="currentColor"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                color="white"
-              />
-              <path
-                d="M23.6361 9.33998C22.212 8.71399 20.6892 8.25903 19.0973 8C18.9018 8.33209 18.6734 8.77875 18.5159 9.13408C16.8236 8.89498 15.1469 8.89498 13.4857 9.13408C13.3283 8.77875 13.0946 8.33209 12.8974 8C11.3037 8.25903 9.77927 8.71565 8.35518 9.3433C5.48276 13.4213 4.70409 17.3981 5.09342 21.3184C6.99856 22.6551 8.84487 23.467 10.66 23.9983C11.1082 23.4189 11.5079 22.8029 11.8523 22.1536C11.1964 21.9195 10.5683 21.6306 9.9748 21.2951C10.1323 21.1856 10.2863 21.071 10.4351 20.9531C14.0551 22.5438 17.9881 22.5438 21.5649 20.9531C21.7154 21.071 21.8694 21.1856 22.0251 21.2951C21.4299 21.6322 20.8 21.9211 20.1442 22.1553C20.4885 22.8029 20.8865 23.4205 21.3364 24C23.1533 23.4687 25.0013 22.6567 26.9065 21.3184C27.3633 16.7738 26.1261 12.8335 23.6361 9.33998ZM12.3454 18.9075C11.2587 18.9075 10.3676 17.9543 10.3676 16.7937C10.3676 15.6331 11.2397 14.6783 12.3454 14.6783C13.4511 14.6783 14.3422 15.6314 14.3232 16.7937C14.325 17.9543 13.4511 18.9075 12.3454 18.9075ZM19.6545 18.9075C18.5678 18.9075 17.6767 17.9543 17.6767 16.7937C17.6767 15.6331 18.5488 14.6783 19.6545 14.6783C20.7602 14.6783 21.6514 15.6314 21.6323 16.7937C21.6323 17.9543 20.7602 18.9075 19.6545 18.9075Z"
-                fill="#5865F2"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
           </button>
         </div>
         <div
-          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 transition-all duration-300 ease-in-out ${
             isMenuOpen ? "block" : "hidden"
           }`}
           id="navbar-cta"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-white">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 md:p-0 text-black hover:text-primary"
+                className="block py-2 px-3 md:p-0 text-black hover:text-primary dark:hover:text-primary"
                 aria-current="page"
               >
                 Home
@@ -70,7 +91,7 @@ export const NavigationBar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 md:p-0 text-black hover:text-primary"
+                className="block py-2 px-3 md:p-0 text-black hover:text-primary dark:hover:text-primary"
               >
                 About
               </a>
@@ -78,7 +99,7 @@ export const NavigationBar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 md:p-0 text-black hover:text-primary"
+                className="block py-2 px-3 md:p-0 text-black hover:text-primary dark:hover:text-primary"
               >
                 Products
               </a>
@@ -86,7 +107,7 @@ export const NavigationBar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 md:p-0 text-black hover:text-primary"
+                className="block py-2 px-3 md:p-0 text-black hover:text-primary dark:hover:text-primary"
               >
                 Contact
               </a>
