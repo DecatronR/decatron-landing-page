22fab573579ad3d7cca98a23abc8ff4e59f503a6
koladeolukoya <koladeolukoya@gmail.com>
2024-08-12 04:00:34 +0100

    feat: contact page

--- src/components/Content/ContactUs.tsx
+++ src/components/Content/ContactUs.tsx
@@ -7,89 +7,12 @@ export const ContactUs = () => {
             Contact us
           </h2>
           <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-500">
-            Get in Touch with Us Today to Elevate Your Recruitment Strategy
+            We want to hear from you.
           </p>
         </div>
 
         <div className="max-w-5xl mx-auto mt-12 sm:mt-16">
           <div className="grid grid-cols-1 gap-6 px-8 text-center md:px-0 md:grid-cols-3">
-            <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
-                <svg
-                  className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                  />
-                </svg>
-                <p className="mt-6 text-lg font-medium text-gray-900">
-                  +1-123-456-1890
-                </p>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  +1-123-456-1890
-                </p>
-              </div>
-            </div>
-
-            <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
-                <svg
-                  className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-                <p className="mt-6 text-lg font-medium text-gray-900">
-                  contact@example.com
-                </p>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  info@example.com
-                </p>
-              </div>
-            </div>
-
-            <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
-                <svg
-                  className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <p className="mt-6 text-lg font-medium leading-relaxed text-gray-900">
-                  8502 Some Rd. SomeWhere, CITY 98380, USA
-                </p>
-              </div>
-            </div>
           </div>
 
           <div className="mt-6 overflow-hidden bg-white rounded-xl">
@@ -126,7 +49,7 @@ export const ContactUs = () => {
                         type="email"
                         name=""
                         id=""
-                        placeholder="Enter your full name"
+                        placeholder="Enter your email address"
                         className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
                       />
                     </div>
@@ -142,7 +65,7 @@ export const ContactUs = () => {
                         type="tel"
                         name=""
                         id=""
-                        placeholder="Enter your full name"
+                        placeholder="Enter your phone number"
                         className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
                       />
                     </div>
@@ -151,16 +74,19 @@ export const ContactUs = () => {
                   <div>
                     <label className="text-base font-medium text-gray-900">
                       {" "}
-                      Company name{" "}
+                      What best describes you{" "}
                     </label>
                     <div className="mt-2.5 relative">
-                      <input
-                        type="text"
+                      <select
                         name=""
                         id=""
-                        placeholder="Enter your full name"
                         className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
-                      />
+                        >
+                        <option value="job-seeker">Property Buyer</option>
+                        <option value="job-seeker">Property Owner</option>
+                        <option value="job-seeker">Agent</option>
+                        <option value="job-seeker">Real Estate Professional</option>
+                        </select>
                     </div>
                   </div>
 
